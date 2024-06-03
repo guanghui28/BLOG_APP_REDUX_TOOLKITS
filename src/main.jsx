c9dbefdaf1e1e4b3cf65adbee209ed00ef030624
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-03 14:51:37 +0700

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
+import { worker } from "./api/server";
+
+(async () => {
+	await worker.start({ onUnhandledRequest: "bypass" });
+})();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
